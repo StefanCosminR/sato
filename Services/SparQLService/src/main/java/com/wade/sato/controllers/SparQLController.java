da317e40884bd8fc4143acc34184b9541f302c4b
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2020-01-03 15:09:05 +0200

    [SparQLService] refactored sparql endpoint

--- Services/SparQLService/src/main/java/com/wade/sato/controllers/SparQLController.java
+++ Services/SparQLService/src/main/java/com/wade/sato/controllers/SparQLController.java
@@ -1,15 +1,11 @@
 package com.wade.sato.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.stardog.stark.query.SelectQueryResult;
-import com.stardog.stark.query.io.QueryResultFormats;
-import com.stardog.stark.query.io.QueryResultWriters;
-import com.wade.sato.adapters.StardogAdapter;
 import com.wade.sato.constants.StardogConfigConstants;
 import com.wade.sato.models.sparql.SparQLQuery;
-import com.wade.sato.models.sparql.SparQLQueryResult;
 import com.wade.sato.models.stardog.StardogConfig;
 import lombok.extern.log4j.Log4j2;
+import org.apache.curator.shaded.com.google.common.net.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,24 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 @Log4j2
 @RestController
 @RequestMapping("/sparql")
 public class SparQLController {
-  private ObjectMapper objectMapper;
-  private StardogAdapter adapter;
+  private String queryLocation;
 
   public SparQLController() throws IOException {
-    this.objectMapper = new ObjectMapper();
-    StardogConfig config = objectMapper.readValue(StardogConfigConstants.CONFIG_FILE, StardogConfig.class);
-    this.adapter = new StardogAdapter(config.getStorageUrl(),
-                                      config.getStorageUsername(),
-                                      config.getStoragePassword(),
-                                      config.getDatabase());
+    StardogConfig config = new ObjectMapper().readValue(StardogConfigConstants.CONFIG_FILE, StardogConfig.class);
+    this.queryLocation = String.format("%s/%s/query", config.getStorageUrl(), config.getDatabase());
   }
 
   @RequestMapping(method = RequestMethod.GET)
@@ -43,19 +40,22 @@ public class SparQLController {
   }
 
   @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity<SparQLQueryResult> query(final @RequestBody SparQLQuery query) {
-    try {
-      SelectQueryResult result = adapter.query(query.getQuery());
-      return new ResponseEntity<>(transformStardogSelectResult(result), HttpStatus.OK);
-    } catch (IOException e) {
-      log.error("Could not retrieve query result: {}", e.getMessage());
-      return new ResponseEntity<>(new SparQLQueryResult(), HttpStatus.SERVICE_UNAVAILABLE);
-    }
-  }
+  public ResponseEntity<String> query(final HttpServletRequest request, final @RequestBody SparQLQuery query) {
+    Client client = ClientBuilder.newClient();
+    Entity<Form> payload = Entity.form(new Form().param("query", query.getQuery()));
 
-  private SparQLQueryResult transformStardogSelectResult(final SelectQueryResult result) throws IOException {
-    ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
-    QueryResultWriters.write(result, resultStream, QueryResultFormats.JSON);
-    return objectMapper.readValue(new ByteArrayInputStream(resultStream.toByteArray()), SparQLQueryResult.class);
+    Response response = client.target(queryLocation)
+        .request(MediaType.TEXT_PLAIN_TYPE)
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+        .header(HttpHeaders.ACCEPT, request.getHeader(HttpHeaders.ACCEPT))
+        .header(HttpHeaders.AUTHORIZATION, "Basic YWRtaW46YWRtaW4=")
+        .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+        .post(payload);
+
+    HttpStatus status = Optional
+        .of(HttpStatus.valueOf(response.getStatus()))
+        .orElse(HttpStatus.SERVICE_UNAVAILABLE);
+
+    return new ResponseEntity<>(response.readEntity(String.class), status);
   }
 }
