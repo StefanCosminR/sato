5c38e1545180decf7ee1278c1c77334ab02042e8
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-03 12:04:41 +0200

    [FrontEnd] Added main pages

--- Services/UserInterface/src/app/app-routing.module.ts
+++ Services/UserInterface/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainPageComponent} from './components/pages/main-page/main-page.component';
 import {SearchPageComponent} from './components/pages/search-page/search-page.component';
+import {MatLinkPreviewModule} from '@angular-material-extensions/link-preview';
+import {NgxLinkifyjsModule} from 'ngx-linkifyjs';
 
 
 const routes: Routes = [
@@ -10,7 +12,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    NgxLinkifyjsModule.forRoot(),
+    MatLinkPreviewModule.forRoot()
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Services/UserInterface/src/app/app.module.ts
+++ Services/UserInterface/src/app/app.module.ts
@@ -8,8 +8,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
 import {FormsModule} from '@angular/forms';
 import { MainMenuBarComponent } from './components/shared/main-menu-bar/main-menu-bar.component';
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import { SearchPageComponent } from './components/pages/search-page/search-page.component';
+import {MatLinkPreviewModule} from '@angular-material-extensions/link-preview';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {NgxLinkifyjsModule} from 'ngx-linkifyjs';
+import {NgxLinkPreviewModule} from 'ngx-link-preview';
 
 @NgModule({
   declarations: [
@@ -28,7 +32,11 @@ import { SearchPageComponent } from './components/pages/search-page/search-page.
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatLinkPreviewModule,
+    NgxLinkifyjsModule,
+    NgxLinkPreviewModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
