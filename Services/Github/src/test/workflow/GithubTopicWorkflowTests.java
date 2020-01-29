3002ec42028fea03c7e62b817afe78b14751ee0a
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2020-01-29 13:59:50 +0200

    Run workflow on all github topics

--- Services/Github/src/test/workflow/GithubTopicWorkflowTests.java
+++ Services/Github/src/test/workflow/GithubTopicWorkflowTests.java
@@ -1,11 +1,16 @@
 package workflow;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import constants.github.GithubConfigConstants;
+import models.config.GithubConfig;
 import models.workflow.GithubTopicWorkflowParams;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class GithubTopicWorkflowTests {
   private static final String URL = "http://localhost:5820";
@@ -24,5 +29,22 @@ public class GithubTopicWorkflowTests {
   @Test
   public void test_github_topic_data_collector_workflow() throws FileNotFoundException {
     workflow.start(TOPIC, true);
+  }
+
+  @Test
+  public void test_run_workflow_on_all_topics() throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    List<String> topics = objectMapper.readValue(GithubConfigConstants.CONFIG_FILE, GithubConfig.class).getTopics();
+
+    topics.forEach(topic -> {
+      try {
+        System.out.println(String.format("Collecting topic: %s", topic));
+        workflow.start(topic, false);
+        System.out.println("Waiting request rate limit reset...");
+        TimeUnit.HOURS.sleep(1);
+      } catch (FileNotFoundException | InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
   }
 }
