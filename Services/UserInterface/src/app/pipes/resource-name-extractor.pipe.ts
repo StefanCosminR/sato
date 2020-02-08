ffdca5643bcf7bedd94fe77769e27504c51235ff
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2020-02-08 20:20:19 +0200

    [UI] Fix suggestion name edge case

--- Services/UserInterface/src/app/pipes/resource-name-extractor.pipe.ts
+++ Services/UserInterface/src/app/pipes/resource-name-extractor.pipe.ts
@@ -5,6 +5,10 @@ export class ResourceNameExtractorPipe implements PipeTransform {
     readonly MAX_NAME_LENGTH = 13;
 
     transform(url: string): string {
+        if (url.endsWith('/')) {
+            url = url.substr(0, url.length - 1);
+        }
+
         const name = url.substr(url.lastIndexOf('/') + 1);
         if (name.length > this.MAX_NAME_LENGTH) {
             return `${name.substr(0, this.MAX_NAME_LENGTH)}...`;
